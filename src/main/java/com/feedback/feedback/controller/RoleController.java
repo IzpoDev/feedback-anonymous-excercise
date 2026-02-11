1f5b0cf30b5642d74422f381f990c9c1d8f186dd
IzpoDev <ponce.1714.frp@gmail.com>
2026-02-11 17:46:52 -0500

    feature: Primera version de la nueva estructura estbale

--- src/main/java/com/feedback/feedback/controller/RoleController.java
+++ src/main/java/com/feedback/feedback/controller/RoleController.java
@@ -1,4 +1,12 @@
 package com.feedback.feedback.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/roles")
+@RequiredArgsConstructor
 public class RoleController {
+
 }
