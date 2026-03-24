3dff361ccd7202be92d669be21e89053dbacbb72
IzpoDev <ponce.1714.frp@gmail.com>
2026-03-24 01:00:06 -0500

    fix

--- src/main/java/com/feedback/feedback/config/SecurityConfig.java
+++ src/main/java/com/feedback/feedback/config/SecurityConfig.java
@@ -37,7 +37,6 @@ public class SecurityConfig {
         // 2. Configurar las reglas de autorización
         http.authorizeHttpRequests(
                 requests -> requests
-                        .requestMatchers(HttpMethod.GET,"/auth/configuration").hasRole("ADMIN")
                         .requestMatchers(
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
@@ -60,6 +59,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/feedbacks/**").hasAuthority("READ_FEEDBACK")
                         .requestMatchers(HttpMethod.GET, "/feedbacks/owner/**").hasAnyRole("ADMIN", "OWNER")
                         .requestMatchers("/privileges/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.GET,"/auth/configuration").hasRole("ADMIN")
 
                         .anyRequest().authenticated()
         );
