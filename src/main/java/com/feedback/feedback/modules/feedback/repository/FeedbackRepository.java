34bdc9648e982b71d7f07e3056c297583e175bcf
IzpoDev <ponce.1714.frp@gmail.com>
2026-04-03 22:51:40 -0500

    fix:Solucion de logic para listar solos feedbacks de los owners activos

--- src/main/java/com/feedback/feedback/modules/feedback/repository/FeedbackRepository.java
+++ src/main/java/com/feedback/feedback/modules/feedback/repository/FeedbackRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Repository
 public interface FeedbackRepository extends JpaRepository<FeedbackEntity, Long> {
 
-    @Query("select f from FeedbackEntity f where f.recipient.id=:id")
+    @Query("select f from FeedbackEntity f where f.recipient.id=:id and f.active=true")
     List<FeedbackEntity> findAllByRecipientId(@Param("id") Long recipientId);
 
 }
